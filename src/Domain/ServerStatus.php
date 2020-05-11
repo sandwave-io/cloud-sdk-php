595c507d0ee278ae0c7c5aef266ad4e1f4871ef7
Tristan Linnenbank <tl@kikvors.com>
2020-05-11 08:50:06 +0200

    Added new status stopped

--- src/Domain/ServerStatus.php
+++ src/Domain/ServerStatus.php
@@ -8,6 +8,7 @@ use InvalidArgumentException;
 final class ServerStatus
 {
     private const STATUS_RUNNING = 'Running';
+    private const STATUS_STOPPED = 'Stopped';
 
     /** @var string  */
     private $value;
@@ -18,12 +19,13 @@ final class ServerStatus
     private function __construct(string $value)
     {
         $options = [
-            ServerStatus::STATUS_RUNNING
+            ServerStatus::STATUS_RUNNING,
+            ServerStatus::STATUS_STOPPED
         ];
 
         if (!in_array($value, $options)) {
             throw new InvalidArgumentException(
-                sprintf('Server status must be one of %s', implode(',', $options))
+                sprintf('Server status must be one of \'%s\', got %s', implode(',', $options), $value)
             );
         }
 
@@ -33,6 +35,11 @@ final class ServerStatus
     public static function running(): ServerStatus
     {
         return new ServerStatus(ServerStatus::STATUS_RUNNING);
+    }
+
+    public static function stopped(): ServerStatus
+    {
+        return new ServerStatus(ServerStatus::STATUS_STOPPED);
     }
 
     public function equals(ServerStatus $other): bool
